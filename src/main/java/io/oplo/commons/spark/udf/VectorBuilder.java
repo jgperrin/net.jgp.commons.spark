f65cf95fdd33bb726dcc28dfd1facd9c0467fd58
Jean Georges Perrin <jgp@jgp.net>
2016-07-25 15:22:51 -0400

    First deployment

---
+++ src/main/java/io/oplo/commons/spark/udf/VectorBuilder.java
@@ -0,0 +1,14 @@
+package io.oplo.commons.spark.udf;
+
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.sql.api.java.UDF1;
+
+public class VectorBuilder implements UDF1<Double, Vector> {
+	private static final long serialVersionUID = -2991355883253063841L;
+
+	public Vector call(Double t1) throws Exception {
+		return Vectors.dense(t1);
+	}
+
+}
